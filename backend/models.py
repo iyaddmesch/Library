c8a8546e1f35a36f8b26863dc787303c3690bd7a
aladinbenz <56277363+aladinbenz@users.noreply.github.com>
2020-06-05 21:43:30 +0200

    Updated models
    normalement hadha tech tahtaj

--- backend/models.py
+++ backend/models.py
@@ -1,60 +1,126 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-Gender = (('', '----'),
-    ('Male','Male'),
-    ('Female','Female'),
-    )
-    
+
+# choice fields to be later placed in a separate file
+# gender
+gender = (('', '----'),
+('Male', 'Male'),
+('Female', 'Female'))
+
+# languages choice
+languages = (('', '---------'),
+('English', 'English'), 
+('Arabic', 'Arabic'), 
+('Danish', 'Danish'), 
+('German', 'German'), 
+('Spanish', 'Spanish'), 
+('French', 'French'),  
+('Italian', 'Italian'), 
+('Japanese', 'Japanese'),  
+('Norwegian', 'Norwegian'), 
+('Polish', 'Polish'), 
+('Portuguese', 'Portuguese'), 
+('Romanian', 'Romanian'), 
+('Russian', 'Russian'))
+
+
+class Author(models.Model):
+    id        = models.CharField(editable=False, primary_key=True, max_length=35)
+    full_name = models.CharField(max_length=30, blank=True)
+    birthday  = models.DataField(blank=True, null=True)
+    Gender    = models.CharField(max_length=6, choices=Gender, default="")
+    picture   = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    phone     = models.constants(max_length=10, blank = false)
+    email     = models.EmailField(max_length=40, blank=True)
+
+    def __str__(self):
+        return str(self.full_name)
+
+
 class Profile(models.Model):
-    user = models.OneToOneField(user,on_delete=models.CASCADE)
-    full_name=models.CharField(max_length=30 , blank=True)
-    description = models.TextField(max_length=300,blank=True)
-    birthday = models.DataField(blank=True, null=True)
-    image = models.ImageField(default='default.jpg',upload_to='profile_pics')
-    Gender = models.CharField(max_length=6, choices=Gender, default="")
-    phone_number = models.IntegerField(max_length=10 , blank = false)
-    Email = models.EmailField(max_length=40 , blank=True)
-    number_of_books_read = models.IntegerField()
-    points = models.IntegerField()
-    book = models.ForeignKey(Book,on_delete=models.CASCADE)
-    prefered_category= models.TextField(max_length=500 , blank=True)
+    id          = models.CharField(editable=False, primary_key=True, max_length=35)
+    full_name   = models.CharField(max_length=30, blank=True)
+    description = models.TextField(max_length=300, blank=True)
+    birthday    = models.DataField(blank=True, null=True)
+    Gender      = models.CharField(max_length=6, choices=Gender, default="")
+    phone       = models.constants(max_length=10, blank = false)
+    email       = models.EmailField(max_length=40, blank=True)
+    preferences = models.TextField(max_length=500, blank=True)
+    picture     = models.ImageField(default='default_profile.jpg', upload_to='profile_pics')
+    books_read  = models.IntegerField(default=0)
+    score       = models.IntegerField(default=0)
+    #relations
+    books       = models.ManyToManyField('Book', related_name='books_authors')
+    user        = models.OneToOneField(User, on_delete=models.CASCADE)
     
+    def __str__(self):
+        return str(self.id)
+
 
 class Book(models.Model):
-    title=models.CharField(max_length=30 , blank=True)
-    language= models.CharField(max_length=20 , blank=True)
-    author = models.CharField(max_length=30, blank=True)
-    category =models.CharField(max_length=60, blank=True)
-    avaible = models.BooleanField()
-    number_of_copies = models.IntegerField(max= 20 , min= 0)
-    description = models.TextField(max_length=500 , blank=True)
-    image =  models.ImageField(default='default.jpg',upload_to='book_pics')
-class Sessions(models.Model):
-    picture = models.ImageField(default='default.jpg',upload_to='book_pics')
-    guest_name = models.CharField(max_length=50 , blank=True )
-    date = models.DataField(blank=True, null=True)
-    info_guest = models.TextField(max_length=500 , blank=True)     
-    reviews = models.TextField(max_length=500, blank=True) 
-    guest_books = models.TextField(max_length=400 , blank=True)
-    session_report = models.TextField(max_length=500 ,blank=True) 
-class Reviews (models.Model):
-    user    = models.ForeignKeyField(user,on_delete=models.CASCADE)
-    book = models.ForeignKey(Book,on_delete=models.CASCADE)
-    sessions = models.ForeignKey(Sessions, on_delete = models.CASCADE)
-    comment = models.TextField(max_length=600 , blank=True)
-    stars = models.IntegerField(max = (5) , min = (1))
-    
-class Challenge (models.Model):
-    user    = models.ForeignKeyField(user, blank = True , null=True,on_delete = models.SET_NULL , related_name = 'answered_by')
-    book = models.ManyToManyField(Book)
-    question = models.TextField(max_length=200, blank=True)
-    answer = models.TextField(max_length=600 , blank=True)
-    evaluation = models.IntegerField()
-class Author(models.Model):
-    user = models.ManyToManyField(Book , on_delete=models.CASCADE)
-    Sessions = models.ManyToManyField(Sessions, on_delete=models.CASCADE)
-    
+    slug        = models.SlugField()
+    isbn        = models.CharField(max_length=30)
+    title       = models.CharField(max_length=30, blank=True)
+    language    = models.CharField(max_length=80, null=True, choices=languages, default="")
+    published   = models.DataField(blank=True, null=True)
+    category    = models.CharField(max_length=60, blank=True )
+    available   = models.BooleanField(default=False)
+    quantity    = models.IntegerField(max=20, min=0)
+    description = models.TextField(max_length=500, blank=True)
+    picture     = models.ImageField(default='default_book.jpg', upload_to='book_picture')
+    #relations
+    authors     = models.ManyToManyField(Author, related_name='authors')
+    review      = models.ManyToManyField(Review, related_name='book_reviews')
 
-    
+    def __str__(self):
+        return ", ".join([str(self.title) + " " + str(self.slug) + " (" + str(self.isbn) +") "])
+
+class Session(models.Model):
+    id      = models.CharField(editable=False, primary_key=True, max_length=35)
+    date    = models.DataField(blank=True, null=True)
+    picture = models.ImageField(default='default_session.jpg', upload_to='book_pics')
+    report  = models.TextField(max_length=500, blank=True) 
+    #relations
+    authors = models.ManyToManyField(Author, related_name='books_authors')
+    reviews = models.ManyToManyField(Review, related_name='session_reviews')
+
+    def __str__(self):
+        return ", ".join([str(self.id) + " " + " (" + str(self.date) +") "])
 
 
+class BookReview(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    comment      = models.TextField(max_length=600 , blank=True)
+    stars        = models.IntegerField(max=5, min=1)
+    #relations
+    review_by    = models.ForeignKeyField(User,on_delete=models.CASCADE)
+    book         = models.ForeignKeyField(Book,on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-date_created']
+
+
+class SessionReview(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    comment      = models.TextField(max_length=600, blank=True)
+    stars        = models.IntegerField(max=5, min=1, default=1)
+    #relations
+    review_by    = models.ForeignKeyField(User, on_delete=models.CASCADE)
+    session      = models.ForeignKeyField(Session, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-date_created']
+
+    
+class challenge(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    answered_by  = models.ForeignKeyField(User, blank=True, null=True, on_delete=models.SET_NULL, related_name='answered_by')
+    book         = models.ManyToManyField(Book, blank=True, related_name='related_books')
+    question     = models.TextField(max_length=200, blank=True)
+    answer       = models.TextField(max_length=600, blank=True)
+    evaluation   = models.IntegerField()
+
+    class Meta:
+        ordering = ['-date_created']
